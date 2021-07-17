b4fc7a2846455e42a59b0ac18f9960861af99cef
Rupender Singh <46915626+rupenderbauhtey@users.noreply.github.com>
2021-07-18 00:40:24 +0530

    Create SumTillNUsingRecursion.cpp

---
+++ SumTillNUsingRecursion.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std; 
+
+int Sum(int n)
+{
+
+    if (n==0)
+    {
+        return 0; 
+    }
+    
+    int prevSum = Sum(n-1); 
+
+    return n + prevSum; 
+
+    
+}
+int main()
+{  
+    int n; 
+    cout<<"Enter the number till which you want to find the sum :"<<endl; 
+    cin>>n; 
+
+    cout<<Sum(n)<<endl; 
+}
